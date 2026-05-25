18e5e973047ff26afbb675ff9360fa14f18c2d5e
Tawfik <tawfikabouhmad.404@gmail.com>
2026-05-25 22:18:22 +0000

    test migrations

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/run-migrations', function () {
+    if (request('token') !== env('MIGRATE_TOKEN')) {
+        abort(403);
+    }
+    
+    Artisan::call('migrate', ['--seed' => true, '--force' => true]);
+    return response()->json([
+        'output' => Artisan::output()
+    ]);
 });

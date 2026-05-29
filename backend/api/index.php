1b14f4897315eced29100cf441f44327daf80ebf
Tawfik <tawfikabouhmad.404@gmail.com>
2026-05-29 21:16:11 +0000

    cache for prod

--- backend/api/index.php
+++ backend/api/index.php
@@ -3,7 +3,6 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-header('Access-Control-Max-Age: 86400');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);

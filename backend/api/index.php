d265899e16d3732e69d4fad65569d6623afd9512
Tawfik <tawfikabouhmad.404@gmail.com>
2026-06-01 08:51:43 +0000

    test fix CORS 3

--- backend/api/index.php
+++ backend/api/index.php
@@ -1,13 +1,14 @@
 <?php
 
-// header('Access-Control-Allow-Origin: *');
-// header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
-// header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+header('Access-Control-Max-Age: 86400');
 
-// if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-//     http_response_code(204);
-//     exit();
-// }
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit();
+}
 $tmpStorage = '/tmp/laravel-storage';
 
 foreach ([
